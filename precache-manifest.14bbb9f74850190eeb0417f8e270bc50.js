cac11916355ce1109a5d6fc13ae7e3c871ffa26c
Teri Eyenike <teyenike1@gmail.com>
2020-10-24 21:40:36 +0100

    Updates

---
+++ precache-manifest.14bbb9f74850190eeb0417f8e270bc50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7026aafa41c6bc9a36d86a93e6bc25",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "0343a98a7814ee93667e",
+    "url": "/task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2d338a84a9109db3bcde",
+    "url": "/task/static/js/2.bd5dbb08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task/static/js/2.bd5dbb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0343a98a7814ee93667e",
+    "url": "/task/static/js/main.8c611448.chunk.js"
+  },
+  {
+    "revision": "f05f3ce3a087fefc40e4",
+    "url": "/task/static/js/runtime-main.1b29c376.js"
+  }
+]);
